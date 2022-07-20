dd1e45347b14867f1c8b34738e1f1606fee90e83
sunergy08 <65669659+sunergy08@users.noreply.github.com>
2022-07-20 03:50:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> name = new Dictionary<string, int>();
+            name.Add("Golden", 1);
+            name.Add("Patricia", 2);
+            name.Add("Damilola", 1);
+            name.Add("Effiong", 2);
+            name.Add("Ajayi", 1);
+
+            // Dictionary elements
+            Console.WriteLine("Dictionary elements: " + name.Count);
+          
+        }
+    }
+}
